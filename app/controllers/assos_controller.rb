0d059d89646cf40968be8e649b89ae4c3438d53c
Estelle Bousquet <estellebousquet@gmail.com>
2021-02-23 17:37:13 +0100

    ok

---
+++ app/controllers/assos_controller.rb
@@ -0,0 +1,9 @@
+class AssosController < ApplicationController
+  def index
+    @assos = Asso.all
+  end
+
+  def show
+    @asso = Asso.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :dashboard, only: [ :show ]
 
-  resources :associations do
+  resources :assos do
     resources :favs, only: [ :new, :create ]
   end
 end
