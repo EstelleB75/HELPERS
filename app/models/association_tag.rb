fda2ff94dba08b7d76592c1ab14828fdac147a15
Estelle Bousquet <estellebousquet@gmail.com>
2021-02-23 13:01:17 +0100

    make validations

--- app/models/association_tag.rb
+++ app/models/association_tag.rb
@@ -1,4 +1,6 @@
 class AssociationTag < ApplicationRecord
   belongs_to :association
   belongs_to :tag
+
+  validates :tag_id, uniqueness: { scope: :association_id }
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ApplicationRecord
   belongs_to :sub_category
   has_many :associations, through: :association_tags
+
+  validates :name, presence: true, uniqueness: true
 end
