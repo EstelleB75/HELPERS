9ef5996559fadc64e09ab781472c2fef3f1a2d04
Kyllian Creppy <kyllian.creppy@outlook.fr>
2021-02-23 16:55:57 +0100

    Controller & view début

--- app/controllers/associations_controller.rb
+++ app/controllers/associations_controller.rb
@@ -1,2 +1,9 @@
 class AssociationsController < ApplicationController
+  def index
+    @associations = Association.all
+  end
+
+  def show
+    @association = Association.find(params[:id])
+  end
 end
