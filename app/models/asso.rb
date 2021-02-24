eb6ebdc194784fff44fdd53b646a4945eb681f85
Estelle Bousquet <estellebousquet@gmail.com>
2021-02-24 11:20:27 +0100

    cloudinary-config

--- app/models/asso.rb
+++ app/models/asso.rb
@@ -1,6 +1,8 @@
 class Asso < ApplicationRecord
   has_many :tags, through: :association_tags
   has_many :favs, dependent: :destroy
+  has_one_attached :photo
+
 
   validates :name, presence: true
   validates :city, presence: true
