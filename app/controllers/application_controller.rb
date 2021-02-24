ef4e9a386d8f03d25e50f3726143e78971de1cd2
Kyllian Creppy <kyllian.creppy@outlook.fr>
2021-02-24 17:55:07 +0100

    Pundit search authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   private
 
   def skip_pundit?
-    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)|(^searchs$)/
   end
 end
