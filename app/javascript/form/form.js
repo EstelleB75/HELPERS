52962c4e5dfad62a282870da948c368d08afea3a
Kyllian Creppy <kyllian.creppy@outlook.fr>
2021-02-25 14:31:56 +0100

    Formulaire js

--- app/javascript/form/form.js
+++ app/javascript/form/form.js
@@ -1,13 +1,23 @@
 // Poser un micro sur le clic
+const hideAllQuestions = () => {
+  const hide = document.querySelectorAll(".question")
+  hide.forEach((question) => {
+    question.classList.add("hidden")
+  }) 
+}
+
+const displayQuestion = (idQuestion) => {
+  hideAllQuestions()
+  const question = document.querySelector(`.question[data-question='${idQuestion}'`)
+  question.classList.remove("hidden")
+
+}
 const handleForm = () => {
-  const buttons = document.querySelectorAll(".question-1 button")
+  const buttons = document.querySelectorAll("button")
   buttons.forEach((button) => {
     button.addEventListener("click", (event) => {
-      const questionNumber = event.target.id
-      const question = document.querySelector(`.${questionNumber}`)
-      question.classList.toggle("hidden")
-      
-      
+      const questionNumber = event.target.dataset.target
+      displayQuestion(questionNumber)
     })
   })
   
@@ -16,7 +26,10 @@ const handleForm = () => {
     button2.addEventListener("click", (event) => {
       const questionNumber2 = event.target.id
       const question2 = document.querySelector(".question-3")
+      const form2 = document.querySelector(".question-2")
+      form2.classList.toggle("hidden")
       question2.classList.toggle("hidden")
+
     })
   })
 }
