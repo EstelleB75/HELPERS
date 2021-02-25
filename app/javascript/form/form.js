9165f45c6315476cd78785f15f191de4def6e0ae
Kyllian Creppy <kyllian.creppy@outlook.fr>
2021-02-25 17:43:09 +0100

    Formulaire partie 1

--- app/javascript/form/form.js
+++ app/javascript/form/form.js
@@ -10,26 +10,35 @@ const displayQuestion = (idQuestion) => {
   hideAllQuestions()
   const question = document.querySelector(`.question[data-question='${idQuestion}'`)
   question.classList.remove("hidden")
-
 }
+
+const back = () => {
+  const container = document.querySelector("#question-container")
+  const history =  JSON.parse(container.dataset.questions)
+  const questionId = history.pop()
+  container.dataset.questions = JSON.stringify(history)
+  console.log(questionId)
+  displayQuestion(history[history.length - 1])
+}
+
 const handleForm = () => {
-  const buttons = document.querySelectorAll("button")
+  const buttons = document.querySelectorAll(".btn-question")
   buttons.forEach((button) => {
     button.addEventListener("click", (event) => {
       const questionNumber = event.target.dataset.target
+      const container = document.querySelector("#question-container")
+      const history =  JSON.parse(container.dataset.questions)
+      history.push(questionNumber)
+      console.log(questionNumber)
+      container.dataset.questions = JSON.stringify(history)
       displayQuestion(questionNumber)
     })
   })
   
-  const buttons2 = document.querySelectorAll(".question-2 button")
-  buttons2.forEach((button2) => {
-    button2.addEventListener("click", (event) => {
-      const questionNumber2 = event.target.id
-      const question2 = document.querySelector(".question-3")
-      const form2 = document.querySelector(".question-2")
-      form2.classList.toggle("hidden")
-      question2.classList.toggle("hidden")
-
+  const backButtons = document.querySelectorAll(".btn-back")
+  backButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      back()
     })
   })
 }
