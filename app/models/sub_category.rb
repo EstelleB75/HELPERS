0f8ff40a6b83459d349695e51e0ec81ba22fc3a0
Estelle Bousquet <estellebousquet@gmail.com>
2021-03-04 11:43:59 +0100

    ok

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -1,4 +1,13 @@
 class SubCategory < ApplicationRecord
   belongs_to :category
   has_many :tags
+
+  def to_sentence_label
+    case name
+    when "Santé" then "blablabla"
+
+    else
+      "other"
+    end
+  end
 end
