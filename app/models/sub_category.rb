07373cff0ace4b1f121c7a807c89854bf7bbabb6
Estelle Bousquet <estellebousquet@gmail.com>
2021-03-04 15:03:11 +0100

    ok

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -4,10 +4,11 @@ class SubCategory < ApplicationRecord
 
   def to_sentence_label
     case name
-    when "Santé" then "blablabla"
-
+    when "Santé" then "Je cherche à résoudre un problème de santé"
+    when "Dons" then "J'ai un besoin nutritionnel, ma recherche concerne l'alimentation"
+    when "Aide à la Personne" then "Je recherche un soutien, une aide à la personne"
     else
-      "other"
+      "Je cherche une activité gratuite, de culture ou de loisirs"
     end
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,16 @@ class Tag < ApplicationRecord
   has_many :assos, through: :association_tags
 
   validates :name, presence: true, uniqueness: true
+
+  def to_sentence_tag
+    case name
+    when "Aide à domicile" then "Je cherche quelqu'un pour m'aider, une aide à domicile"
+    when "Soin" then "J'ai besoin de consulter un médecin, de faire des soins médicaux"
+    when "Mobilité" then "Je sollicite une aide mobilité, j'ai besoin de me déplacer"
+    when "Lien social" then "Je recherche un organisme social, un entourage de proximité"
+    when "Service" then "J'ai besoin d'un service spécifique à un problème domestique"
+    else
+    "Je consulte les prises en charge dédiées aux handicaps"
+    end
+  end
 end
