94f1da144fad2a8059f1ced244d2497e1dc84968
Mathieu Trancoso <Trancoso.mathieu@gmail.com>
2022-12-11 19:53:33 +0100

    removefromroutes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   get 'search_sub_cat', to: 'searchs#search_sub_cat'
   get 'search_tag', to: 'searchs#search_tag'
   get 'search_address', to: 'searchs#search_address' 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'dashboard', to: 'dashboards#profile'
   get 'results_asso', to: 'assos#results'
 
